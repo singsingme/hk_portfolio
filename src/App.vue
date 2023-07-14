ddfd7e9acf360202801aa14a409b28f1746aaa8b
singsingme <103347724+singsingme@users.noreply.github.com>
2023-07-14 20:40:22 +0900

    background

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- template 에 컴포넌트 등록을 해 줘야지 쓸 수 있음 -->
+    <Content></Content>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Content from "@/Content.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Content,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
